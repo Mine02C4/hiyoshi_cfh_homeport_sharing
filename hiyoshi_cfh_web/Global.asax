d938842cd5c15a2d00b322d7a0ef3cd9f5d37f96
naoya <mine@mine-studio.com>
2015-06-06 02:21:18 +0900

    Initial commit
    Generate solution and project by ASP.NET template

---
+++ hiyoshi_cfh_web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="hiyoshi_cfh_web.MvcApplication" Language="C#" %>
---
+++ hiyoshi_cfh_web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(hiyoshi_cfh_web.Startup))]
+namespace hiyoshi_cfh_web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
