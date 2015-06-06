70de25dd2cf0c58580dcea10c32ffdfd83561c13
naoya <mine@mine-studio.com>
2015-06-07 06:04:21 +0900

    hiyoshi_cfh_client プロジェクトの作成

---
+++ hiyoshi_cfh_client/ClientView.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace hiyoshi_cfh_client
+{
+    /// <summary>
+    /// ClientView.xaml の相互作用ロジック
+    /// </summary>
+    public partial class ClientView : UserControl
+    {
+        public ClientView()
+        {
+            InitializeComponent();
+        }
+
+        private async void Send(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
