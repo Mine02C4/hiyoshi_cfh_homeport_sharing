335a5210510e473c03114cbfe319a7f22f5320a4
naoya <mine@mine-studio.com>
2015-06-07 06:22:49 +0900

    提督業も忙しい！とのインターフェース部の仮実装

--- hiyoshi_cfh_client/HiyoshiCfhClient.cs
+++ hiyoshi_cfh_client/HiyoshiCfhClient.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using Grabacr07.KanColleViewer.Composition;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.Composition;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace hiyoshi_cfh_client
 {
-    public class HiyoshiCfhClient
+    [Export(typeof(IToolPlugin))]
+    [ExportMetadata("Title", "日吉連合艦隊司令部クライアント")]
+    [ExportMetadata("Description", "日吉連合艦隊司令部のシステムを利用するためのクライアントです。")]
+    [ExportMetadata("Version", "1.0")]
+    [ExportMetadata("Author", "@mine_studio")]
+    public class HiyoshiCfhClient : IToolPlugin
     {
+        public object GetToolView()
+        {
+            return new ClientView();
+        }
+
+        public string ToolName
+        {
+            get { return "日吉"; }
+        }
+
+        public object GetSettingsView()
+        {
+            return null;
+        }
     }
 }
