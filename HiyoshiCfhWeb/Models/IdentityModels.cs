1d70e3329560b41df892f7ddde5dd71c1fa0b047
naoya <mine@mine-studio.com>
2015-06-07 23:52:58 +0900

    .NETの命名規則にしたがってプロジェクト名と名前空間を変更(Web)
    今更ながら非常に違和感を感じたので変更

---
+++ HiyoshiCfhWeb/Models/IdentityModels.cs
@@ -0,0 +1,39 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace HiyoshiCfhWeb.Models
+{
+    // ApplicationUser クラスにプロパティを追加することでユーザーのプロファイル データを追加できます。詳細については、http://go.microsoft.com/fwlink/?LinkID=317594 を参照してください。
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // authenticationType が CookieAuthenticationOptions.AuthenticationType で定義されているものと一致している必要があります
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // ここにカスタム ユーザー クレームを追加します
+            return userIdentity;
+        }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public DbSet<Admiral> Admirals { get; set; }
+        public DbSet<Ship> Ships { get; set; }
+        public DbSet<ShipInfo> ShipInfoes { get; set; }
+        public DbSet<ShipType> ShipTypes { get; set; }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
--- hiyoshi_cfh_web/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="hiyoshi_cfh_web.MvcApplication" Language="C#" %>
