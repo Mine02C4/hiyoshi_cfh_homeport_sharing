8de345bffd165f464bf7f679916abc431f206619
naoya <mine@mine-studio.com>
2015-06-29 17:25:04 +0900

    パスワード認証周りのViewModelを削除

--- HiyoshiCfhWeb/Models/AccountViewModels.cs
+++ HiyoshiCfhWeb/Models/AccountViewModels.cs
@@ -53,60 +53,7 @@ namespace HiyoshiCfhWeb.Models
         [EmailAddress]
         public string Email { get; set; }
 
-        //[Required]
-        //[DataType(DataType.Password)]
-        //[Display(Name = "パスワード")]
-        //public string Password { get; set; }
-
         [Display(Name = "このアカウントを記憶する")]
         public bool RememberMe { get; set; }
-    }
-
-    public class RegisterViewModel
-    {
-        [Required]
-        [EmailAddress]
-        [Display(Name = "電子メール")]
-        public string Email { get; set; }
-
-        [Required]
-        [StringLength(100, ErrorMessage = "{0} の長さは {2} 文字以上である必要があります。", MinimumLength = 6)]
-        [DataType(DataType.Password)]
-        [Display(Name = "パスワード")]
-        public string Password { get; set; }
-
-        [DataType(DataType.Password)]
-        [Display(Name = "パスワードの確認入力")]
-        [Compare("Password", ErrorMessage = "パスワードと確認のパスワードが一致しません。")]
-        public string ConfirmPassword { get; set; }
-    }
-
-    public class ResetPasswordViewModel
-    {
-        [Required]
-        [EmailAddress]
-        [Display(Name = "電子メール")]
-        public string Email { get; set; }
-
-        [Required]
-        [StringLength(100, ErrorMessage = "{0} の長さは {2} 文字以上である必要があります。", MinimumLength = 6)]
-        [DataType(DataType.Password)]
-        [Display(Name = "パスワード")]
-        public string Password { get; set; }
-
-        [DataType(DataType.Password)]
-        [Display(Name = "パスワードの確認入力")]
-        [Compare("Password", ErrorMessage = "パスワードと確認のパスワードが一致しません。")]
-        public string ConfirmPassword { get; set; }
-
-        public string Code { get; set; }
-    }
-
-    public class ForgotPasswordViewModel
-    {
-        [Required]
-        [EmailAddress]
-        [Display(Name = "電子メール")]
-        public string Email { get; set; }
     }
 }
