940dcedcbc7991d778f0d718afb389b02b0d3a09
naoya <mine@mine-studio.com>
2015-07-09 07:43:45 +0900

    ウェブ経由でプラグインを配布

---
+++ HiyoshiCfhWeb/Controllers/PluginController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HiyoshiCfhWeb.Controllers
+{
+    public class PluginController : Controller
+    {
+        // GET: Plugin
+        public ActionResult Index()
+        {
+            var files = Directory.GetFiles(Server.MapPath("~/Content/Plugin")).Select(x => new FileInfo(x));
+            return View(files);
+        }
+    }
+}
