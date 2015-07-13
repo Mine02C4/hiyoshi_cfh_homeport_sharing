e58b7dd014134c2d338bb40f13616e05f1a2d3c8
naoya <mine@mine-studio.com>
2015-07-14 06:02:16 +0900

    Zipとしてプラグインをダウンロードする機能を追加

--- HiyoshiCfhWeb/Controllers/PluginController.cs
+++ HiyoshiCfhWeb/Controllers/PluginController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,11 +10,45 @@ namespace HiyoshiCfhWeb.Controllers
 {
     public class PluginController : Controller
     {
+        private string PluginPath
+        {
+            get
+            {
+                return Server.MapPath("~/Content/Plugin");
+            }
+        }
+
+        private IEnumerable<FileInfo> PluginFiles
+        {
+            get
+            {
+                return Directory.GetFiles(PluginPath).Select(x => new FileInfo(x));
+            }
+        }
+
         // GET: Plugin
         public ActionResult Index()
         {
-            var files = Directory.GetFiles(Server.MapPath("~/Content/Plugin")).Select(x => new FileInfo(x));
-            return View(files);
+            return View(PluginFiles);
+        }
+
+        public ActionResult Zipped()
+        {
+            var memoryStream = new MemoryStream();
+            using (var zip = new ZipArchive(memoryStream, ZipArchiveMode.Create))
+            {
+                foreach (var file in PluginFiles)
+                {
+                    var entry = zip.CreateEntry(file.Name);
+                    entry.LastWriteTime = TimeZoneInfo.ConvertTimeFromUtc(file.LastWriteTimeUtc,
+                        TimeZoneInfo.FindSystemTimeZoneById("Tokyo Standard Time"));
+                    using (var writer = new BinaryWriter(entry.Open()))
+                    {
+                        writer.Write(System.IO.File.ReadAllBytes(file.FullName));
+                    }
+                }
+            }
+            return File(memoryStream.ToArray(), "application/zip", "HiyoshiCfhClient.zip");
         }
     }
 }
