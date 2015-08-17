88a63bac21330ec2a2c37f34204747ce7dadc219
naoya <mine@mine-studio.com>
2015-08-18 01:53:25 +0900

    パスワード関連のViewModelを削除

--- HiyoshiCfhWeb/Models/ManageViewModels.cs
+++ HiyoshiCfhWeb/Models/ManageViewModels.cs
@@ -20,44 +20,6 @@ namespace HiyoshiCfhWeb.Models
         public IList<AuthenticationDescription> OtherLogins { get; set; }
     }
 
-    public class FactorViewModel
-    {
-        public string Purpose { get; set; }
-    }
-
-    public class SetPasswordViewModel
-    {
-        [Required]
-        [StringLength(100, ErrorMessage = "{0} の長さは {2} 文字以上である必要があります。", MinimumLength = 6)]
-        [DataType(DataType.Password)]
-        [Display(Name = "新しいパスワード")]
-        public string NewPassword { get; set; }
-
-        [DataType(DataType.Password)]
-        [Display(Name = "新しいパスワードの確認入力")]
-        [Compare("NewPassword", ErrorMessage = "新しいパスワードと確認のパスワードが一致しません。")]
-        public string ConfirmPassword { get; set; }
-    }
-
-    public class ChangePasswordViewModel
-    {
-        [Required]
-        [DataType(DataType.Password)]
-        [Display(Name = "現在のパスワード")]
-        public string OldPassword { get; set; }
-
-        [Required]
-        [StringLength(100, ErrorMessage = "{0} の長さは {2} 文字以上である必要があります。", MinimumLength = 6)]
-        [DataType(DataType.Password)]
-        [Display(Name = "新しいパスワード")]
-        public string NewPassword { get; set; }
-
-        [DataType(DataType.Password)]
-        [Display(Name = "新しいパスワードの確認入力")]
-        [Compare("NewPassword", ErrorMessage = "新しいパスワードと確認のパスワードが一致しません。")]
-        public string ConfirmPassword { get; set; }
-    }
-
     public class AddPhoneNumberViewModel
     {
         [Required]
