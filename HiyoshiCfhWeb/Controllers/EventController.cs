091e905c7c653d8ce806bfc79d119f119ffcc43a
naoya <mine@mine-studio.com>
2015-09-03 10:31:00 +0900

    イベント新艦娘の入状況一覧

---
+++ HiyoshiCfhWeb/Controllers/EventController.cs
@@ -0,0 +1,20 @@
+﻿using HiyoshiCfhWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HiyoshiCfhWeb.Controllers
+{
+    public class EventController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Event/e2015summer
+        public ActionResult e2015summer()
+        {
+            return View(db.Admirals.ToList());
+        }
+    }
+}
