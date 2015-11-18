8c2c65d704e818643f518968a9f277d245f49ed3
naoya <mine@mine-studio.com>
2015-11-19 06:13:03 +0900

    装備をODataに公開

---
+++ HiyoshiCfhWeb/Controllers/OData/SlotItemInfoesController.cs
@@ -0,0 +1,16 @@
+﻿using HiyoshiCfhWeb.Models;
+using System.Linq;
+using System.Web.Http;
+
+namespace HiyoshiCfhWeb.Controllers.OData
+{
+    [Authorize]
+    public class SlotItemInfoesController : ODataControllerBase<SlotItemInfo>
+    {
+        public SlotItemInfoesController()
+            : base()
+        {
+            dbs = db.SlotItemInfoes;
+        }
+    }
+}
---
+++ HiyoshiCfhWeb/Controllers/OData/SlotItemsController.cs
@@ -0,0 +1,16 @@
+﻿using HiyoshiCfhWeb.Models;
+using System.Linq;
+using System.Web.Http;
+
+namespace HiyoshiCfhWeb.Controllers.OData
+{
+    [Authorize]
+    public class SlotItemsController : ODataControllerBase<SlotItem>
+    {
+        public SlotItemsController()
+            : base()
+        {
+            dbs = db.SlotItems;
+        }
+    }
+}
