3e346dc088d5b7d505e52cca2850e7c064ecbec0
naoya <mine@mine-studio.com>
2016-06-01 12:02:04 +0900

    一時的に出撃識別札の変更を無効化

--- HiyoshiCfhWeb/Controllers/OData/ShipsController.cs
+++ HiyoshiCfhWeb/Controllers/OData/ShipsController.cs
@@ -1,6 +1,8 @@
 ﻿using HiyoshiCfhWeb.Models;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Web.Http;
+using System.Web.OData;
 
 namespace HiyoshiCfhWeb.Controllers
 {
@@ -19,6 +21,85 @@ namespace HiyoshiCfhWeb.Controllers
                 else
                     return false;
             };
+        }
+
+        // PUT: odata/Ships(5)
+        public override IHttpActionResult Put([FromODataUri] int key, Delta<Ship> patch)
+        {
+            Validate(patch.GetEntity());
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var obj = dbs.Find(key);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            int? sortieTag = obj.SortieTag;
+            patch.Put(obj);
+            obj.SortieTag = sortieTag;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!Exists(key))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Updated(obj);
+        }
+
+        // PATCH: odata/Ships(5)
+        [AcceptVerbs("PATCH", "MERGE")]
+        public override IHttpActionResult Patch([FromODataUri] int key, Delta<Ship> patch)
+        {
+            Validate(patch.GetEntity());
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var obj = dbs.Find(key);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            int? sortieTag = obj.SortieTag;
+            patch.Patch(obj);
+            obj.SortieTag = sortieTag;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!Exists(key))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Updated(obj);
         }
     }
 }
